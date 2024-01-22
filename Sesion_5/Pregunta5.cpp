74eb850fdf4b7c77512395cc671ff34e2db89919
jair.0801 <jair.080106@gmail.com>
2024-01-22 15:30:15 -0500

    Sesion_5

---
+++ Sesion_5/Pregunta5.cpp
@@ -0,0 +1,27 @@
+//realice un algoritmo 
+//que permita determinar pago a la compañia y lo que debe pagar cada alumno por viaje
+#include <iostream>
+using namespace std;
+
+int main(){
+    int alumnos;
+
+    cout <<"Ingrese el numero de alumnos";
+    switch (alumnos)
+    {
+    case () :
+        cout <<"Cada alumnos paga 65 soles y el costo de renta del bus es de "<<alumnos*65<<endl;
+        break ;
+    case () :
+        cout <<"Cada alumnos paga 70 soles y el costo de renta del bus es de "<<alumnos*70<<endl;
+        break ;
+    case () :
+        cout <<"Cada alumnos paga 95 soles y el costo de renta del bus es de "<<alumnos*95<<endl;
+        break ;
+    case () :
+        cout <<"Cada alumnos paga "<<4000/alumnos<< "soles y el costo de renta del bus es de "<<4000<<endl;
+        break ;
+    default:
+        break;
+    }
+}
